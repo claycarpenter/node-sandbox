3e8d2e9347bff7592eaa59697816603481282cc2
Clay Carpenter <claycarpenter@gmail.com>
2016-09-03 18:02:10 -0400

    Adds Slider, refs example.

--- react/react-fundamentals/App.js
+++ react/react-fundamentals/App.js
@@ -1,29 +1,58 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+
 
 class App extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      txt: 'this is the state txt',
+      red: 0,
+      green: 0,
+      blue: 0,
     };
   }
 
   update(e) {
-    this.setState({txt: e.target.value});
+    this.setState({
+      red: ReactDOM.findDOMNode(this.refs.red.refs.input).value,
+      green: ReactDOM.findDOMNode(this.refs.green.refs.input).value,
+      blue: ReactDOM.findDOMNode(this.refs.blue.refs.input).value,
+    });
   }
 
   render() {
     console.log('render');
-    
+
     return (
       <div>
-        <input type="text" onChange={this.update.bind(this)}/>
-        <div>{this.state.txt}</div>
+        <pre>{JSON.stringify(this.state)}</pre>
+        <hr/>
+        <Slider ref="red" update={this.update.bind(this)} />
       </div>
     );
   }
 }
+
+class Slider extends React.Component {
+  render() {
+    return (
+      <div>
+        <input ref="input" type="range" min="0" max="255" onChange={this.props.update} />
+        <h4>{this.props.txt}</h4>
+      </div>
+    );
+  }
+}
+
+const Widget = (props) => {
+  return (
+    <div>
+      <input type="text" onChange={props.update}/>
+      <h4>{props.txt}</h4>
+    </div>
+  );
+};
 
 // App.propTypes = {
 //   txt: React.PropTypes.string,
